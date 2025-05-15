e6d9e3922fe958b34d2a5a4b01cfcee845ec2c22
fiquett <fiquett@users.noreply.github.com>
2025-05-15 20:23:53 +0000

    deploy: da56f9d07ec471e3945c2296bfb098e3a8e2ced6

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Cfpb Data Privacy Rollback",
-        "excerpt":" ","categories": [],
-        "tags": [],
+        "title": "Consent Is Not Optional: The Weaponization of Personal Data in Post-Democratic America",
+        "excerpt":"Consent Is Not Optional: The Weaponization of Personal Data in Post-Democratic America **By Brandon Fiquett Sentient Commons May 15, 2025** Abstract In May 2025, the U.S. Consumer Financial Protection Bureau (CFPB) officially withdrew a proposed rule that would have enforced consent requirements for the sale of sensitive personal data by...","categories": [],
+        "tags": ["data rights","surveillance","CFPB","data brokers","deregulation","national security","consent","press release"],
         "url": "/2025/05/15/cfpb-data-privacy-rollback.html",
         "teaser": null
       }]

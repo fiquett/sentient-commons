d892e08862d89bb6c44f3173322455c63db2b7d3
fiquett <fiquett@users.noreply.github.com>
2025-05-15 20:06:56 +0000

    deploy: 0332e7899f1aa5eb15ecf722c08275fd8ef383ae

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Cfpb Data Privacy Rollback",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/2025/05/15/cfpb-data-privacy-rollback.html",
+        "teaser": null
+      }]

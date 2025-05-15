6d3f5aeafee3843bb224bf62dc1ae65f8d4436e1
fiquett <fiquett@users.noreply.github.com>
2025-05-15 20:37:56 +0000

    deploy: 79b2d402429197af8909badde2904711a4663e7f

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["data rights","surveillance","CFPB","data brokers","deregulation","national security","consent","press release"],
         "url": "/2025/05/15/cfpb-data-privacy-rollback.html",
         "teaser": null
+      },{
+        "title": "They’re Selling You. And They Don’t Even Need Your Permission.",
+        "excerpt":"They’re Selling You. And They Don’t Even Need Your Permission. Imagine this: A stranger knows your income, your credit score, your home address, your location at 2:36 PM yesterday, who you voted for, what religion you left, and whether you’re pregnant. Now imagine you never told them. Because you didn’t....","categories": [],
+        "tags": ["data brokers","privacy","surveillance capitalism","CFPB","deregulation","digital rights"],
+        "url": "/2025/05/15/consent-is-not-optional.html",
+        "teaser": null
       }]
